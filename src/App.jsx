aa1e20f75b11132b1cd896842af1afd2ed8098ed
Sagar <sagar.kapoor@vyld.io>
2024-07-19 14:38:43 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
+import SkipBox from './components/SkipBox'
 import './App.css'
 import Landingpage from './pages/Landingpage'
 function App() {
-
+const [skip,setskip]=useState(false);
 
   return (
     <>
-    <Landingpage/>
+      {skip &&<SkipBox skip={skip} setskip={setskip}/>}
+    <Landingpage skip={skip} setskip={setskip}/>
     </>
   )
 }
